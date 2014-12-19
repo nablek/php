15a6517bc0ba5ad465c6e080681bf774a6a09a97
北山彩貴子 <160307w@shinoLab7.local>
2014-12-19 14:53:39 +0900

    test

---
+++ testoutput.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<title>output.php</title>
+</head>
+<body>
+<?php
+  $name = $_POST['name'];
+  print ("次のデータを受け取りました<br />");
+  print ("名前：$name<br />");
+?>
+</body>
+</html>
