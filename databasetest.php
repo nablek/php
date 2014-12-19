66202766d0cd7b5b6927d9bba1e61dbe0c56a016
160303s <160303s@ugs.kochi-tech.ac.jp>
2014-12-19 14:36:19 +0900

    データベース接続PHP
    trafficテーブルに格納されているデータの呼び出しおよび表示ができるPHP文を上げておきます。

---
+++ databasetest.php
@@ -0,0 +1,40 @@
+<html>
+<head><title>PHP DATABASE TEST</title></head>
+<body>
+
+<?php
+
+// Mysqlのユーザ情報入力
+$user = 'root';
+$password = '';
+
+// データベース接続
+try {;
+	// systemというデータベースに接続，IPアドレスはlocalhost，user,passwordは変数を呼び出し
+	$dbh = new PDO('mysql:dbname=system;host=localhost', $user, $password);
+
+	echo "データベース接続完了 <br>";
+
+	// sqlの文字コード設定　（必須）
+	$dbh->query('SET NAMES utf8');
+
+	// 実際のsql文，今回はtrafficテーブルから全内容を呼び出した
+    $sql = 'select * from traffic';
+    // 呼び出した内容の出力，呼び出した内容を変数$rowに一時格納
+    foreach ($dbh->query($sql) as $row) {
+    	// データテーブル内のカラムを指定して出力
+        print($row['station_id'].' ');
+        print($row['station_name'].'<br>');
+    }
+// 例外処理
+} catch (PDOException $e) {
+	exit('データベースに接続できませんでした。' . $e->getMessage());
+}
+
+// データベース切断
+$pdo = null;
+
+?>
+
+</body>
+</html>
