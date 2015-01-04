7770d10b957238739cdc6e45b2adb8f24ba73f01
160303s <160303s@ugs.kochi-tech.ac.jp>
2015-01-04 12:55:04 +0900

    お勧め物件更新PHP変更
    遅れて申し訳ない。
    
    データベースに合うように設定を変更。
    ただし、画像付属処理は完全に飛ばしています。
    残りやらないといけないことは、空白を開けてコメント文にしているので、それをやってください。

--- jukyoeve2.php
+++ jukyoeve2.php
@@ -14,41 +14,42 @@
 		<tr>
 		<th>見出し</th>
 		<td><input type="text" name="event_name" size="30"></td>
-				</tr>
+		</tr>
 
-<!-- 場所入力欄 -->
-<tr>
-<th>場所(住所)</th>
-<td><input type="text" name="event_place" size="30"></td>
-</tr>
-	
-<!-- 電話番号入力欄 -->
+		<!-- 開催日程入力欄 -->
 		<tr>
-		<th>電話番号</th>
-				<td><input type="tel" name="telephone" size="30" maxlength="17" placeholder="00-0000-0000" pattern="\d{1,5}-\d{1,4}-\d{4,5}" title="市外局番からハイフン(-)を入れて記入してください。" required></td>
-						</tr>
-						 
-    			<!-- 内容入力欄 -->
-    			<tr>
-            		<th>内容</th>
-            		<td><textarea rows="10" cols="50" name="content"></textarea></td>
-            		</tr>
-            		</table>
-            		 
-            		<!-- 画像挿入欄 -->
-            		<tr>
-            		<!-- 下の一文があるとページの遷移ができないため、一応コメントアウトしています -->
-            	<!-- <form action="#" method="post" enctype="multipart/form-data"> -->
-            		<p>
-            		<input type="file" name="filename">
-        	</p>
-        	<!-- </form> -->
-        	</tr>
-        	<!-- 送信ボタン -->
-        	<div id="mainform-submit">
-        	<!-- nameを送信ボタンから「register」へ、,valueを送信から「確認画面へ」に変更しました。  -->
-        	<input type="submit" name="register" value="確認画面へ">
-        	</div>
-        	</form>
+		<th>開催日程</th>
+		<td><input type="text" name="event_date" size="30"></td>
+		</tr>
+
+		<!-- 場所入力欄 -->
+		<tr>
+		<th>場所(住所)</th>
+		<td><input type="text" name="event_place" size="30"></td>
+		</tr>
+
+		<!-- 内容入力欄 -->
+		<tr>
+		<th>内容</th>
+		<td><textarea rows="10" cols="50" name="content"></textarea></td>
+		</tr>
+</table>
+
+		<!-- 画像挿入欄 -->
+		<tr>
+		<!-- 下の一文があるとページの遷移ができないため、一応コメントアウトしています -->
+		<!-- <form action="#" method="post" enctype="multipart/form-data"> -->
+		<p>
+		<input type="file" name="filename">
+		</p>
+		<!-- </form> -->
+		</tr>
+		
+		<!-- 送信ボタン -->
+		<div id="mainform-submit">
+		<!-- nameを送信ボタンから「register」へ、,valueを送信から「確認画面へ」に変更しました。  -->
+		<input type="submit" name="register" value="確認画面へ">
+		</div>
+</form>
 </body>
 </html>
--- jukyoeve3.php
+++ jukyoeve3.php
@@ -1,64 +1,77 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>answer test</title>
+<title>おすすめ物件更新完了</title>
 </head>
 <body>
 
 <?php
 
-        //Mysqlのユーザ情報入力
-        $user ='root';
-        $password = '';
+	//Mysqlのユーザ情報入力
+	$user ='root';
+	$password = '';
 
-        //各項目の値を取得
-        $evename = $_POST['event_name'];
-        $eveplace = $_POST['event_place'];
-        $tel = $_POST['telephone'];
-        $contents = $_POST['content'];
-        $image = $_POST['filename'];
-        
-        //必須項目に記入がない場合のエラー
-        if ($evename == false){
-        	die ("※（必須）見出しを入力してください");
-        }else if ($eveplace == false){
-        	die ("※（必須）場所を入力してください");
-        }else if ($tel == false){
-        	die ("※（必須）電話番号を入力してください");
-        }else if ($contents == false){
-        	die ("※（必須）内容を入力してください");
-        }
-       
+	//各項目の値を取得
+	$eventname = $_POST['event_name'];
+	$eventdate = $_POST ['event_date'];
+	$eventplace = $_POST['event_place'];
+	$content = $_POST['content'];
+	$image = $_POST['filename'];
+
+
+
+
+
+
+
+
+	//$user_idの取得方法の検討！、event_dateがテーブルではdate(0000-00-00)型になっているので入力画面側もdate型に合わせること
+
+	//テスト用に無理やりuser_idを設定、ログイン時のuser_idを取得し$user_id変数に代入することで以下のSQLは正常に動作
+	//ただし、挿入画像に関してはデータベースにカラムを設定していないため今回は書いていない
+	$user_id = "JA";
+
+
+
+
+
+
+
+	//必須項目に記入がない場合のエラー
+	if ($eventname == false){
+		die ("※（必須）見出しを入力してください");
+	}else if ($eventdate == false) {
+		die ("※（必須）見出しを入力してください");
+	}else if ($eventplace == false){
+		die ("※（必須）場所を入力してください");
+	}else if ($content == false){
+		die ("※（必須）内容を入力してください");
+	}
+
 try {
-	   // ★データベース関連はここから↓
-	   // データベースに接続（データベース名は未記入、,IPアドレス→localhost, userとpasswordは変数呼び出し）
-	   $dbh = new PDO('mysql:dbname=データベース名;host=localhost',$user, $password);
-	   
-	   //sqlの文字コード設定
-	   $dbh->query('SET NAMES utf8');
+	// ★データベース関連はここから↓
+	// データベースに接続（データベース名は未記入、,IPアドレス→localhost, userとpasswordは変数呼び出し）
+	$dbh = new PDO('mysql:dbname=system;host=localhost',$user, $password);
 
-	   // SQL文をそれぞれの変数に格納(DB名,テーブル名,フィールド名は未記入)
-	
-	/* $eventname_sql = "INSERT INTO 'DB名'.'テーブル名'('フィールド名') VALUES('$event_name');";
-	 $eventplace_sql = "INSERT INTO 'DB名'.'テーブル名'('フィールド名') VALUES('$event_place');";
-	 $telephone_sql = "INSERT INTO 'DB名'.'テーブル名'('フィールド名') VALUES('$telephone');";
-	 $content_sql = "INSERT INTO 'DB名'.'テーブル名'('フィールド名') VALUES('$content');"; 
-	   $image_sql = "INSERT INTO 'DB名'.'テーブル名'('フィールド名') VALUES('$filename');"; */
-	$sql = "INSERT INTO 'DB名'.'テーブル名'
-	VALUES ('$event_name', '$event_place', '$telephone', '$content', '$image');";
-	
+	//sqlの文字コード設定
+	$dbh->query('SET NAMES utf8');
+
+	// SQL文をそれぞれの変数に格納(DB名,テーブル名,フィールド名は未記入)
+
+	// 挿入sql文 inquiry_idはmysql側で自動的に追加されるので必要なし
+	$sql = 'UPDATE area SET event_name = "'.$eventname.'", event_date = "'.$eventdate.'", event_place = "'.$eventplace.'", content =  "'.$content.'" WHERE user_id = "'.$user_id.'"';
+
+
 	$dbh->query($sql);
 	
 	//エラーがない場合結果を表示
 	print("以下の内容で登録します。<br /><br />");
-	print("見出し：$evename<br />");
-	print("場所：$eveplace<br />");
-	print("電話番号：$tel<br />");
-	print("内容：$contents<br />");
+	print("見出し：$eventname<br />");
+	print("開催日時 : $eventdate<br />");
+	print("場所：$eventplace<br />");
+	print("内容：$content<br />");
 	print("挿入画像:$image<br />");
-	//入力完了後の遷移処理
-	
-	
+
 	//例外処理
 } catch (PDOEXception $e) {
 	exit('データベースに接続できませんでした。' . $e->getMessage());
@@ -70,4 +83,16 @@ $pdo = null;
 //メッセージの出力
 echo "登録完了しました";
 
-?>	
+
+
+
+
+//登録完了後のページ遷移処理を何かしら設定しないと画面が登録完了で止まる
+
+
+
+
+
+?>
+
+</body>
