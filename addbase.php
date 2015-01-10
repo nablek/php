668862b190ca627434fa9ffdb4e712d0c05e40e0
だるメシアン <haradamisato@haruda-no-MacBook-Air.local>
2015-01-10 21:55:44 +0900

    店舗の基本情報の入力フォーム、確認画面、登録完了画面です。（DBはいじっていません）

---
+++ addbase.php
@@ -0,0 +1,169 @@
+<!-- 入力フォーム -->
+
+<!-- 入力フォームでの実際の動き -->
+<!-- 1.今回いろんな方法を試しましたが、エラーメッセージの表示に関してはjavascriptで表示するようにしました。 -->
+<!--    →(javascript)未記入の場合、ポップアップでエラーメッセージが出力されます。 -->
+<!-- 2.もし、確認画面へ遷移するボタンをクリックした時にエラー出力があった場合、今まで入力した内容は消えることなくそのまま表示され続けるように設定しました。 -->
+<!-- 3.追加した情報を確認画面で確認できるようになるのは、全部の入力欄が埋まった時のみとしています。 -->
+<!-- 4.確認画面へ進む際は、「確認画面へ」というボタンをクリックすると確認画面へ遷移できるようになっています。 -->
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>基本情報更新ページ</title>
+
+<!-- style sheetの設定 -->
+<style type="text/css">
+<!--
+#header, #footer {
+  background-color: #fed;
+  height: 60px;
+  padding: 10px;
+}
+#main {
+  background-color: #eee;
+  padding: 10px;
+}
+div#conten {
+  text-align: center;
+}
+-->
+</style>
+
+<!-- ↓ここからjavascriptでのエラーメッセージ表示の処理 -->
+<script type="text/javascript">
+<!--
+function form_check() {
+    var flag = 0;
+    if (document.base_form.shop_name.value == "") {   // 店舗名の入力欄が未記入の時
+		flag = 1;
+		window.alert("店舗名が未記入です。");    //表示（ポップアップ）
+    	//return false;
+    }
+    
+    if (document.base_form.shop_phonetic.value == "") {  // 店舗名（ふりがな）の入力欄が未記入の場合
+	    flag = 1;
+		window.alert("店舗名(ふりがな)が未記入です。");    //表示（ポップアップ）
+    	//return false;
+    } 
+    
+    if (document.base_form.address.value == "") {   // 住所の入力欄が未記入の場合
+		flag = 1;
+		window.alert("住所が未記入です。");   //表示（ポップアップ）
+    	//return false;
+    }
+    
+    if (document.base_form.telephone.value == "") {  // 電話番号の入力欄が未記入の場合
+		flag = 1;
+		window.alert("電話番号が未記入です。");   //表示（ポップアップ）
+    	//return false;
+    }
+
+    if (document.base_form.time.value == "") {  // 営業時間の入力欄が未記入の場合
+		flag = 1;
+		window.alert("営業時間が未記入です。");   //表示（ポップアップ）
+    	//return false;
+    }
+
+    if (document.base_form.close.value == "") {  // 定休日の入力欄が未記入の場合
+		flag = 1;
+		window.alert("定休日が未記入です。");   //表示（ポップアップ）
+    	//return false;
+    }
+
+    if (document.base_form.link.value == "") {  // URLの入力欄が未記入の場合
+		flag = 1;
+		window.alert("URLが未記入です。");   //表示（ポップアップ）
+    	//return false;
+    }
+  
+    if (flag) {
+    	//window.alert("必須項目に謝りがあります。");
+    	return false;
+    } else {
+    	return true;
+    }
+}
+-->
+</script>
+
+<!-- ここから入力フォームのhtml -->
+</head>
+<body>
+
+<div id="header">
+<h1>基本情報更新ページ</h1>
+</div>
+
+<div id="content">
+
+<!-- actionのところのphpファイル名は確認画面のphpである「baseconfirm.php」に設定 -->
+  <form  action="baseconfirm.php" name="base_form" method="post" enctype="multipart/form-data" onSubmit="return form_check()">
+    <table class="sample-table">
+		<!-- 店舗名入力欄 -->
+		<tr>
+		<th>店舗名</th>
+		<!-- valueのところに、見出し入力欄に入力した内容がエラーなどが起きてもデータを保持できるようにphpで設定。以下「開催日程」「場所」「内容」においても同様。 -->
+		<td><input type="text" name="shop_name" size="30" value="<?php echo $_POST["shop_name"] ?>">
+		</td>
+		</tr>
+
+		<!-- 店舗名（ふりがな）入力欄 -->
+		<tr>
+		<th>店舗名（ふりがな）</th>
+		<td><input type="text" name="shop_phonetic" size="30" value="<?php echo $_POST["shop_phonetic"] ?>">
+		</td>
+		</tr>
+
+		<!-- 住所入力欄 -->
+		<tr>
+		<th>住所</th>
+		<td><input type="text" name="address" size="30" value="<?php echo $_POST["address"] ?>">  
+		</td>
+		</tr>
+
+		<!-- 電話番号入力欄 -->
+		<tr>
+		<th>電話番号</th>
+		<td><input type="date" name="telephone" size="30" placeholder=" 0000-00-0000" value="<?php echo $_POST["telephone"] ?>">  
+		</td>
+		</tr>
+		
+		<!-- 営業時間入力欄 -->
+		<tr>
+		<th>営業時間</th>
+		<td><input type="text" name="time" size="30" placeholder="0:00~0:00" value="<?php echo $_POST["time"] ?>">  
+		</td>
+		</tr>
+		
+		<!-- 定休日入力欄 -->
+		<tr>
+		<th>定休日</th>
+		<td><input type="text" name="close" size="30" value="<?php echo $_POST["close"] ?>">  
+		</td>
+		</tr>
+		
+		<!-- URL入力欄 -->
+		<tr>
+		<th>URL</th>
+		<td><input type="text" name="link" placeholder="http://www.△△△・・・" value="<?php echo $_POST["link"] ?>">  
+		</td>
+		</tr>
+  </table>
+
+		<!-- 送信ボタン -->
+		<div id="mainform-submit">
+		<!-- nameを送信ボタンから「register」へ、,valueを送信から「確認画面へ」に変更しました。  -->
+		<input type="submit" name="register" value="確認画面へ">
+		</div>
+    </form>
+</div>
+
+<!-- トップページに遷移するためのボタンを設置 -->
+<div id="footer">
+  <a href="top.php">トップページ</a>
+</div>
+
+</body>
+</html>
+
---
+++ baseconfirm.php
@@ -0,0 +1,76 @@
+<!-- 追加した情報の確認画面 -->
+
+<!-- 確認画面での実際の動き -->
+<!-- 1.ここでは前ページの入力フォームで入力した内容にエラーがなかった場合、ここで一度入力した内容を登録を行うために確認できるようになっています。 -->
+<!-- 2.もし、書いた内容に間違いがあり訂正を行いたい場合は「戻る」ボタンを押すと、もう一度入力フォームの方に戻ります。 -->
+<!-- 3.書いた内容に特に訂正がない場合は「登録」ボタンを押してもらうと、登録完了画面に遷移します。 -->
+
+<!-- 入力フォームからのデータの受け渡し -->
+<?php
+//$_POSTを使用して入力した各データを表示用変数にそれぞれ格納
+   $view_shopname = $_POST["shop_name"];
+   $view_phonetic = $_POST["shop_phonetic"];
+   $view_address = $_POST["address"];
+   $view_tel = $_POST["telephone"];
+   $view_time = $_POST["time"];
+   $view_close = $_POST["close"];
+   $view_link = $_POST["link"];
+?>
+
+<!-- ここから確認画面のHTML -->
+   <html>
+   <head>
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+   <title> 確認ページ </title>
+   </head>
+   <body>
+   
+<!-- style sheetの設定 -->
+   <style>
+#header, #footer {
+  background-color: #cff;
+  height: 60px;
+  padding: 10px;
+}
+#main {
+  background-color: #eee;
+  padding: 10px;
+}
+</style>
+      
+   <div align="center">
+   <div id="header">
+<h1>追加情報確認ページ</h1>
+   </div>
+
+<!-- actionのところのphpファイル名は登録完了画面のphpである「basecomplete.php」に設定 -->
+   <form action="basecomplete.php" method="POST">
+   以下の内容で登録します。修正がある場合は「戻る」を、この内容でよい場合は「登録」を押してください。<br><br>
+   
+   <!-- DBに格納するための値を登録完了画面の方で受け渡ししないといけないため、nameを設定。出力はphpで出力させる。 -->
+   店舗名:<input type="hidden"  name="shop_name" value=$view_shopname><?php echo $view_shopnamename; ?><br>
+   <br/>
+   店舗名(ふりがな)：<input type="hidden" name="shop_phonetic" value=$view_phonetic><?php echo $view_phonetic; ?><br>
+   <br/>
+   住所：<input type="hidden"  name="address" value=$view_address><?php echo $view_address; ?><br>
+   <br/>
+   電話番号：<input type="hidden"  name="telphone" value=$view_tel><?php echo $view_tel; ?><br>
+  <br/>
+   営業時間:<input type="hidden"  name="time" value=$view_time><?php echo $view_time; ?><br>
+   <br/>
+   定休日:<input type="hidden"  name="close" value=$view_close><?php echo $view_close; ?><br>
+   <br/>
+   URL:<input type="hidden"  name="link" value=$view_link><?php echo $view_link; ?><br>
+   <br/>
+   
+   <input type="button" value="戻る" onclick="history.go(-1)">
+   <input type="submit" name="set" value="登録" onclick="location.href='basecomplete.php'"> 
+   </form>
+ 
+ <!-- トップページに遷移するためのボタンを設置 -->  
+   <div id="footer">
+  <a href="top.php">トップページ</a>
+   </div>
+   </div>
+   </body>
+   </html>
