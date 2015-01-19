1cc938eeffc2a96d142291b833a5f8ef3f183de2
160303s <160303s@ugs.kochi-tech.ac.jp>
2015-01-19 17:06:40 +0900

    商用イベント変更
    ファイルの内容が学内学外イベントの内容になっていたのですべて修正しました。申し訳ない。

--- add_event_business.php
+++ add_event_business.php
@@ -1,134 +1,138 @@
-	<!-- 入力フォーム -->
+<!-- 入力フォーム -->
 
-	<!DOCTYPE html>
-	<html>
-  	<head>
-    	<meta charset="UTF-8">
-    	<title>イベント情報追加更新ページ</title>
-    
-    	<!-- style sheetの設定 -->
-    	<style type="text/css">
-      	<!--
-	  	#header, #footer {
-	  	background-color: #fed;
-	 	height: 60px;
-	  	padding: 10px;
-	  	}
-	  	#main {
-	  	background-color: #eee;
-	  	padding: 10px;
-	  	}
-	  	div#conten {
-	  	text-align: center;
-	  	}
-		-->
-    	</style>
- 
-    	<!-- ここから入力フォームのhtml -->
-	</head>
-  	<body>
-    
-    	<?php
-       	session_start();
-       	$user_id = $_SESSION['id'];
-       	if ($_SESSION['id'] == null) {
-       	header("Location: ".'/php/loginform.php');
-       	} else if ($_SESSION['visited'] != 2) {
-       	header("Location: ".'/php/top.php');
-       	}
-       
-       	//Mysqlのユーザ情報入力
-       	$user ='root';
-       	$password = '';
-       
-       	try {
-       	// データベースに接続（データベース名は未記入、,IPアドレス→localhost, userとpasswordは変数呼び出し）
-       	$dbh = new PDO('mysql:dbname=system;host=localhost',$user, $password);
-       	
-       	//sqlの文字コード設定
-       	$dbh->query('SET NAMES utf8');
-    
-    	// user_idがデータベースのあるかないかを確認する
-    	$sql = "SELECT user_id as id FROM area WHERE user_id = '$user_id'";
-    	$result = $dbh->query($sql)->fetchAll();
-    
-    	if (empty($result[0]['id']))
-    	{
-    	print("先に店舗の基本情報を入力してください");
-    	?>
-    	<a href="add_base_business.php">ココをクリック</a>
-    	<?php
-       	die();
-       	}
-       
-       	$dbh = null;
-       	//例外処理
-       	} catch (PDOEXception $e) {
-       	exit('データベースに接続できませんでした。' . $e->getMessage());
-    	}
-    	
-	?>
-    	<div align="center">
-      	<div id="content">
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>イベント情報追加更新ページ</title>
+
+<!-- style sheetの設定 -->
+<style type="text/css">
+<!--
+#header, #footer {
+  background-color: #fed;
+  height: 60px;
+  padding: 10px;
+}
+#main {
+  background-color: #eee;
+  padding: 10px;
+}
+div#conten {
+  text-align: center;
+}
+-->
+</style>
+
+
+<!-- ここから入力フォームのhtml -->
+</head>
+
+<body>
+
+<?php
+	session_start();
+	$user_id = $_SESSION['id'];
+	if ($_SESSION['id'] == null) {
+		header("Location: ".'/php/loginform.php');
+	} else if ($_SESSION['visited'] != 2) {
+		header("Location: ".'/php/top.php');
+	}
+
+	//Mysqlのユーザ情報入力
+	$user ='root';
+	$password = '';
 	
-		<!-- actionのところのphpファイル名は確認画面のphpである「conf_business.php」に設定 -->
-		<form  action="conf_event_business.php" method="post" enctype="multipart/form-data">
-	  
-	  	<div id="header">
-	    	<h1>おすすめ情報追加更新</h1>
-	  	</div>
+try {
+	// データベースに接続（データベース名は未記入、,IPアドレス→localhost, userとpasswordは変数呼び出し）
+	$dbh = new PDO('mysql:dbname=system;host=localhost',$user, $password);
 
-	  	<table class="sample-table">
-	    	<!-- 見出し入力欄 -->
-	    	<tr>
-	      	<th>見出し</th>
-	      	<td><input type="text" name="event_name" size="30" autofocus required aria-required="true" x-moz-errormessage="見出しが未記入です" value="<?php echo $_POST["event_name"] ?>">
-	      	</td>
-	    	</tr>
-	    
-	    	<!-- 開催日程入力欄 -->
-	    	<tr>
-	      	<th>開催日程</th>
-	      	<td><input type="text" name="event_date" size="30" placeholder=" 0月00日~0月00日 0:00~0:00" autofocus required aria-required="true" x-moz-errormessage="開催日程が未記入です" value="<?php echo $_POST["event_date"] ?>">
-	      	</td>
-	    	</tr>
-	    
-	    	<!-- 場所入力欄 -->
-	    	<tr>
-	      	<th>場所(住所)</th>
-	      	<td><input type="text" name="event_place" size="30" autofocus required aria-required="true" x-moz-errormessage="場所（住所）が未記入です" value="<?php echo $_POST["event_place"] ?>">  
-	      	</td>
-	    	</tr>
-	    
-	    	<!-- 内容入力欄 -->
-	    	<tr>
-	      	<th>内容</th>
-	      	<td><textarea rows="10" cols="50" name="content" autofocus required aria-required="true" x-moz-errormessage="内容が未記入です"><?php echo $_POST["content"] ?></textarea>
-	      	</td>
-	      
-	    	</tr>
-	    	<!-- ファイルアップロード欄 -->
-	    	<tr>
-	      	<th>ファイルアップロード</th>
-	      	<td><input type="file" name="upfile" size="30"/></td>
-	    	</tr>
-	   	 	<p>
-	  	</table>
-	  	<p>
-	    	<!-- 送信ボタン -->
-	    	<div id="mainform-submit">
-	      	<!-- nameを送信ボタンから「register」へ、,valueを送信から「確認画面へ」に変更しました。  -->
-	      	<input type="submit" name="register" value="確認画面へ">
-	    	</div>
+	//sqlの文字コード設定
+	$dbh->query('SET NAMES utf8');
+
+	// user_idがデータベースのあるかないかを確認する
+	$sql = "SELECT user_id as id FROM area WHERE user_id = '$user_id'";
+	$result = $dbh->query($sql)->fetchAll();
+
+	if (empty($result[0]['id']))
+	{
+		print("先に店舗の基本情報を入力してください");
+?>
+		<a href="add_base_business.php">ココをクリック</a>
+<?php
+		die();
+	}
+	
+	$dbh = null;
+//例外処理
+} catch (PDOEXception $e) {
+	exit('データベースに接続できませんでした。' . $e->getMessage());
+}
+
+?>
+<div align="center">
+
+	<div id="content">
+
+	<!-- actionのところのphpファイル名は確認画面のphpである「conf_business.php」に設定 -->
+		<form  action="conf_event_business.php" name="busi_form" method="post" enctype="multipart/form-data" onSubmit="return form_check()">
+
+			<div id="header">
+				<h1>おすすめ情報追加更新</h1>
+			</div>
+
+			<table class="sample-table">
+				<!-- 見出し入力欄 -->
+				<tr>
+				<th>イベント名</th>
+				<td><input type="text" name="event_name" size="30" autofocus required aria-required="true" x-moz-errormessage="見出しが未記入です" value="<?php echo $_POST["event_name"] ?>">
+				</td>
+				</tr>
+
+				<!-- 開催日程入力欄 -->
+				<tr>
+				<th>開催日程</th>
+				<td><input type="text" name="event_date" size="30"  autofocus required aria-required="true" x-moz-errormessage="開催日程が未記入です" placeholder=" 0月00日~0月00日 0:00~0:00" value="<?php echo $_POST["event_date"] ?>">
+				</td>
+				</tr>
+
+				<!-- 場所入力欄 -->
+				<tr>
+				<th>場所(住所)</th>
+				<td><input type="text" name="event_place" size="30" autofocus required aria-required="true" x-moz-errormessage="場所（住所）が未記入です" value="<?php echo $_POST["event_place"] ?>">  
+				</td>
+				</tr>
+
+				<!-- 内容入力欄 --> 
+				<tr>
+				<th>内容</th>
+				<td><textarea rows="10" cols="50" name="content" autofocus required aria-required="true" x-moz-errormessage="内容が未記入です"><?php echo $_POST["content"] ?></textarea>
+				</td>
+
+				</tr>
+				<!-- ファイルアップロード欄 -->
+				<tr>
+				<th>ファイルアップロード</th>
+				<td><input type="file" name="upfile" size="30"/></td>
+				</tr>
+				<p>
+			</table>
+			<p>
+			<!-- 送信ボタン -->
+			<div id="mainform-submit">
+				<!-- nameを送信ボタンから「register」へ、,valueを送信から「確認画面へ」に変更しました。  -->
+				<input type="submit" name="register" value="確認画面へ">
+			</div>
 		</form>
-      	</div>
-	<p>
-  	<!-- トップページに遷移するためのボタンを設置 -->
-  	<div id="footer">
-    	<a href="top.php">トップページ</a>
-  	</div>
-  
-    	</div>
-    
-  	</body>
-	</html>
+	</div>
+<p>
+<!-- トップページに遷移するためのボタンを設置 -->
+<div id="footer">
+  <a href="top.php">トップページ</a>
+</div>
+
+</div>
+
+</body>
+</html>
+
--- conf_event_business.php
+++ conf_event_business.php
@@ -1,137 +1,135 @@
-	<!-- 追加した情報の確認画面 -->
+<!-- 追加した情報の確認画面 -->
 
-	<!-- 確認画面での実際の動き -->
-	<!-- 1.ここでは前ページの入力フォームで入力した内容にエラーがなかった場合、ここで一度入力した内容を登録を行うために確認できるようになっています。 -->
-	<!-- 2.もし、書いた内容に間違いがあり訂正を行いたい場合は「戻る」ボタンを押すと、もう一度入力フォームの方に戻ります。 -->
-	<!-- 3.書いた内容に特に訂正がない場合は「登録」ボタンを押してもらうと、登録完了画面に遷移します。 -->
+<!-- 確認画面での実際の動き -->
+<!-- 1.ここでは前ページの入力フォームで入力した内容にエラーがなかった場合、ここで一度入力した内容を登録を行うために確認できるようになっています。 -->
+<!-- 2.もし、書いた内容に間違いがあり訂正を行いたい場合は「戻る」ボタンを押すと、もう一度入力フォームの方に戻ります。 -->
+<!-- 3.書いた内容に特に訂正がない場合は「登録」ボタンを押してもらうと、登録完了画面に遷移します。 -->
 
-	<!-- 入力フォームからのデータの受け渡し -->
-	<?php
-	//$_POSTを使用して入力した各データを表示用変数にそれぞれ格納
-		session_start();
-	
-		if ($_SESSION['$visited'] == 2) {
-			$view_category = "学内";
-		} else {
-			$view_category = "学外";
-		}
-		$view_sponame = $_POST["sponsor_name"];
-		$view_mail = $_POST["mailaddress"];
-		$view_evename = $_POST["event_name"];
-		$view_evedate = $_POST["event_date"];
-		$view_eveplace = $_POST["event_place"];
-		$view_evecontent = $_POST["event_content"];
-		$view_file = $_FILES["upfile"]["name"];
-
-		$user_id = $_SESSION['id'];
-		if ($_SESSION['id'] == null) {
-			header("Location: ".'/php/loginform.php');
-		} else if ($_SESSION['visited'] == 2 || $_SESSION['visited'] == 3) {
-			header("Location: ".'/php/top.php');
-		}
-
-	try {
-		// データベースに接続（データベース名は未記入、,IPアドレス→localhost, userとpasswordは変数呼び出し）
-		$dbh = new PDO('mysql:dbname=system;host=localhost',$user, $password);
-
-		//sqlの文字コード設定
-		$dbh->query('SET NAMES utf8');
-
-		// user_idがデータベースのあるかないかを確認する
-		$sql = "SELECT user_id as id FROM inoutside_event WHERE user_id = '$user_id'";
-		$result = $dbh->query($sql)->fetchAll();
-
-		if (empty($result[0]['id'])) {
-			// 初めて登録しているときuserpictureフォルダにuser_idのフォルダを作成
-			// user_id名のフォルダにファイルを格納する
-			$path = '../userpicture/'.$user_id.'';
-			mkdir($path,0755);
-		}
-
-		//データベース切断
-		$dbh = null;
-
-	//例外処理
-	} catch (PDOEXception $e) {
-		exit('データベースに接続できませんでした。' . $e->getMessage());
+<!-- 入力フォームからのデータの受け渡し -->
+<?php
+//$_POSTを使用して入力した各データを表示用変数にそれぞれ格納
+	session_start();
+	if ($_SESSION['$visited'] == 2) {
+		$view_category = "学内";
+	} else if ($_SESSION['$visited'] == 3){
+		$view_category = "学外";
+	} else {
+		$view_category = "管理者";
 	}
-		// ファイルの格納（本来であれば次の格納で行うべきではあるが次のPHPをまたぐと一時ファイルが消えるため仕方ない
-		if (is_uploaded_file($_FILES['upfile']['tmp_name'])) {
-			if (move_uploaded_file($_FILES['upfile']['tmp_name'], "../userpicture/" .$user_id ."/" .$_FILES['upfile']['name'])) {
-				chmod("../userpicture/" .$user_id ."/" .$_FILES['upfile']['name'], 0644);
-			}
-		}
-	?>
+	
+// イベント情報  
+	$view_evename = $_POST["event_name"];
+	$view_evedate = $_POST["event_date"];
+	$view_eveplace = $_POST["event_place"];
+	$view_content = $_POST["content"];
+	$view_file = $_FILES["upfile"]["name"];
 
-	<!-- ここから確認画面のHTML -->
-   	<html>
-   	<head>
-   	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-   	<title> 確認ページ </title>
-   	</head>
-   	<body>
-   
-	<!-- style sheetの設定 -->
-   	<style>
+	$user_id = $_SESSION['id'];
+	if ($_SESSION['id'] == null) {
+		header("Location: ".'/php/loginform.php');
+	} else if ($_SESSION['visited'] == 1) {
+		header("Location: ".'/php/top.php');
+	}
+	// Mysqlのアカウント情報
+	$user = 'root';
+	$password = '';
+
+try {
+	// データベースに接続（データベース名は未記入、,IPアドレス→localhost, userとpasswordは変数呼び出し）
+	$dbh = new PDO('mysql:dbname=system;host=localhost',$user, $password);
+
+	//sqlの文字コード設定
+	$dbh->query('SET NAMES utf8');
+
+	// user_idがデータベースのあるかないかを確認する
+	$sql = "SELECT user_id as id FROM inoutside_event WHERE user_id = '$user_id'";
+	$result = $dbh->query($sql)->fetchAll();
+
+	if (empty($result[0]['id'])) {
+		// 初めて登録しているときuserpictureフォルダにuser_idのフォルダを作成
+		// user_id名のフォルダにファイルを格納する
+		$path = '../userpicture/'.$user_id.'';
+		mkdir($path,0755);
+	}
+
+	//データベース切断
+	$dbh = null;
+
+//例外処理
+} catch (PDOEXception $e) {
+	exit('データベースに接続できませんでした。' . $e->getMessage());
+}
+	// ファイルの格納（本来であれば次の格納で行うべきではあるが次のPHPをまたぐと一時ファイルが消えるため仕方ない
+	if (is_uploaded_file($_FILES['upfile']['tmp_name'])) {
+		if (move_uploaded_file($_FILES['upfile']['tmp_name'], "../userpicture/" .$user_id ."/" .$_FILES['upfile']['name'])) {
+			chmod("../userpicture/" .$user_id ."/" .$_FILES['upfile']['name'], 0644);
+		}
+	}
+?>
+
+<!-- ここから確認画面のHTML -->
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title> 確認ページ </title>
+</head>
+<body>
+
+<!-- style sheetの設定 -->
+<style>
 	#header, #footer {
-  	background-color: #cff;
-  	height: 60px;
-  	padding: 10px;
+		background-color: #cff;
+		height: 60px;
+		padding: 10px;
 	}
 	#main {
-  	background-color: #eee;
-  	padding: 10px;
+		background-color: #eee;
+		padding: 10px;
 	}
-	</style>
+</style>
    
- 	<div align="center">  
- 	以下の内容で登録します。<br>修正がある場合は「戻る」を、この内容でよい場合は「登録」を押してください。<br><br> 
- 	</div>
-     
-   	<div align="center">
-   	<div id="header">
-	<h1>基本情報確認</h1>
-   	</div>
-	<?php
-	print <<<EOM
-	<!-- actionのところのphpファイル名は登録完了画面のphpである「basecomplete.php」に設定 -->
-   	<form action="comp_gakuioeve.php" method="POST">
-   
-   	<!-- DBに格納するための値を登録完了画面の方で受け渡ししないといけないため、nameを設定。出力はphpで出力させる。 -->
-   	<input type="hidden"  name="category" value="$view_category">
-   	主催者名:<input type="hidden"  name="sponsor_name" value="$view_sponame">$view_sponame<br>
-   	<br/>
-   	メールアドレス：<input type="hidden" name="mailaddress" value="$view_mail">$view_mail<br>
-   	<br/>
-   	イベント名：<input type="hidden"  name="event_name" value="$view_evename">$view_evename<br>
-   	<br/>
-   	イベント名：<input type="hidden"  name="event_date" value="$view_evedate">$view_evedate<br>
-   	<br/>
-   	開催場所:<input type="hidden" name="event_place" value="$view_eveplace">$view_eveplace<br>
-   	<br/>
-   	開催場所:<input type="hidden" name="event_content" value="$view_evecontent">$view_evecontent<br>
-   	<br/>
-   	アップロードファイル:<input type="hidden"  name="upfile" value="<?php echo $view_file ?>"><?php echo $view_file; ?><br>
-   	<br/>
-   	<input type="submit" name="set" value="登録"> 
-   	</form>     
+<div align="center">  
+以下の内容で登録します。<br>修正がある場合は「戻る」を、この内容でよい場合は「登録」を押してください。<br><br> 
+</div>
+
+<div align="center">
+<div id="header">
+<h1>おすすめ情報確認</h1>
+</div>
+
+<?php
+print <<<EOM
+<!-- actionのところのphpファイル名は登録完了画面のphpである「basecomplete.php」に設定 -->
+<form action="comp_event_business.php" method="POST">
+
+	<!-- DBに格納するための値を登録完了画面の方で受け渡ししないといけないため、nameを設定。出力はphpで出力させる。 -->
+	イベント名:<input type="hidden"  name="event_name" value=$view_evename>$view_evename<br>
+	<br/>
+	開催日程：<input type="hidden" name="event_date" autofocus required aria-required="true" x-moz-errormessage="開催日程が未記入です" value=$view_evedate>$view_evedate<br>
+	<br/>
+	場所（住所）：<input type="hidden" name="event_place" autofocus required aria-required="true" x-moz-errormessage="場所（住所）が未記入です" value=$view_eveplace>$view_eveplace<br>
+	<br/>
+	内容：<input type="hidden" name="content" autofocus required aria-required="true" x-moz-errormessage="内容が未記入です" value=$view_content>$view_content<br>
+	<br/>
+	アップロードファイル:<input type="hidden" name="upfile" value="$view_file">$view_file<br>
+	<br/>
+	<input type="submit" name="set" value="登録"> 
+</form>     
 EOM;
 ?>
 
-   	<form action="add_base_business.php" method="POST">
-   	<input type="hidden"  name="sponsor_name" value="<?php echo $view_sponame ?>">
-   	<input type="hidden"  name="mailaddress" value="<?php echo $view_mail ?>">
-   	<input type="hidden"  name="event_name" value="<?php echo $view_evename ?>">
-   	<input type="hidden"  name="event_date" value="<?php echo $view_evedate ?>">
-   	<input type="hidden"  name="event_place" value="<?php echo $view_eveplace ?>">
-   	<input type="hidden"  name="event_content" value="<?php echo $view_evecontent ?>">
-   	<input type="submit" value="戻る">
-   	</form>
+	<form action="add_event_business.php" method="POST">
+		<input type="hidden"  name="event_name" value="<?php echo $view_evename ?>">
+		<input type="hidden"  name="event_date" value="<?php echo $view_evedate ?>">
+		<input type="hidden"  name="event_place" value="<?php echo $view_eveplace ?>">
+		<input type="hidden"  name="event_content" value="<?php echo $view_evecontent ?>">
+		<input type="submit" value="戻る">
+	</form>
   
- 	<!-- トップページに遷移するためのボタンを設置 -->  
-   	<div id="footer">
-  	<a href="top.php">トップページ</a>
-   	</div>
-   	</div>
-   	</body>
-   	</html>
+<!-- トップページに遷移するためのボタンを設置 -->  
+<div id="footer">
+	<a href="top.php">トップページ</a>
+</div>
+</div>
+</body>
+</html>
